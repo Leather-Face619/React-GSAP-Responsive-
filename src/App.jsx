ff47c12267a365bf4ad2274f20b38e53731008b0
Leather-Face619 <rahul852369hhhhh@gmail.com>
2025-09-07 17:59:21 +0530

    1st Commit setup page structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (<>
+    <div>App</div>
+    <div className=' bg-red-100'>App</div>
+    <div>App</div>
+    <div>App</div>
+    <div>App</div>
+    <div>App</div>
+  </>
+  )
+}
+
+export default App
