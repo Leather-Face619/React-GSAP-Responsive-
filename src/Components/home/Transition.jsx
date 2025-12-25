0e73cee12e6cb68802e9e1b21e5e143ba2ca2ce2
Leather-Face619 <rahul852369hhhhh@gmail.com>
2025-12-25 20:45:09 +0530

    Remove large video file

---
+++ src/Components/home/Transition.jsx
@@ -0,0 +1,61 @@
+
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { useRef } from 'react'
+import { useLocation } from 'react-router-dom'
+const Transition = (props) => {
+    const parentStairRef = useRef()
+    const pageRef = useRef(null)
+    const currentPath = useLocation().pathname
+
+  useGSAP(function () {
+    const tl = gsap.timeline()
+    tl.to(parentStairRef, {
+      display: 'block',
+    })
+    tl.from('.stair', {
+      height: 0,
+      stagger: {
+        amount: -0.6,
+      },
+
+    })
+      .to('.stair', {
+        y: '100%',
+        stagger: {
+          amount: 0.6,
+        },
+
+      })
+    tl.to(parentStairRef.current, {
+      display: 'none',
+    })
+    tl.to('.stair', {
+      y: '0%'
+    })
+    gsap.from(pageRef.current,{
+      opacity:0,delay:1.2,
+      // scale:1.2,
+    
+    })
+  },[currentPath])
+  return (
+   <>
+   <div ref={parentStairRef} className='fixed h-screen w-full z-50 top-0 left-0 '>
+        <div className='h-full flex w-full ' >
+          <div className='stair h-full w-1/6 bg-black'></div>
+          <div className='stair h-full w-1/6 bg-black'></div>
+          <div className='stair h-full w-1/6 bg-black'></div>
+          <div className='stair h-full w-1/6 bg-black'></div>
+          <div className='stair h-full w-1/6 bg-black'></div>
+          <div className='stair h-full w-1/6 bg-black'></div>
+        </div>
+      </div>
+      <div ref={pageRef}>
+      {props.children}
+      </div>
+   </>
+  )
+}
+
+export default Transition
