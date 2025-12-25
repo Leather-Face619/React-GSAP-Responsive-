147801785457d4ac33ccd28feade5b6b24392af9
Leather-Face619 <rahul852369hhhhh@gmail.com>
2025-12-25 20:52:15 +0530

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,15 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agencs from './pages/Agencs'
 import Projects from './pages/projects'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+
 
 const App = () => {
   
-  useGSAP(function(){
-    const tl = gsap.timeline()
-    tl.from('.stair',{
-      height:0,
-      stagger:{
-        amount:-0.6,
-      },
-      
-    })
-    .to('.stair',{
-      y:'100%',
-      stagger:{
-        amount:0.6,
-      },
-      
-    })
-  })
+  
   return (
   <div className='text-white'>
     
-   <div className='h-screen flex w-screen fixed z-50'>
-    <div className='stair h-full w-1/6 bg-black'></div> 
-    <div className='stair h-full w-1/6 bg-white'></div>
-    <div className='stair h-full w-1/6 bg-black'></div>
-    <div className='stair h-full w-1/6 bg-white'></div>
-    <div className='stair h-full w-1/6 bg-black'></div>
-    <div className='stair h-full w-1/6 bg-white'></div>
-   </div>
+   
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/Agencs' element={<Agencs/>}/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import Transition from './components/home/Transition'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Transition>
     <App />
+    </Transition>
     </BrowserRouter>
 
   </StrictMode>,
